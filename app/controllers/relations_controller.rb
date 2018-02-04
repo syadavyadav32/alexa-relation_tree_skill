f681f5cabc6f92b5cc70aac3a25fb406d303c4c8
Shubham Shiva Yadav <syadav.yadav32@gmail.com>
2018-02-04 15:43:59 +0530

    added alexa verifier

--- app/controllers/relations_controller.rb
+++ app/controllers/relations_controller.rb
@@ -1,9 +1,12 @@
 class RelationsController < ApplicationController
-	
+	skip_before_action :verify_authenticity_token, only: :index
+
 	def index
 		#AlexaVerifier.configure do |config|
   			#config.enabled = false # Disables all checks, even though we enable them individually below
 		#end
+		AlexaVerifier.valid!(request)
+
 		
 		render json: Ralyxa::Skill.handle(request)
 	end
