1139acb12b37625d0e408d397e407191168d4fa7
Shubham Shiva Yadav <syadav.yadav32@gmail.com>
2018-02-05 20:48:28 +0530

    modified intents file

--- intents/add_relation.rb
+++ intents/add_relation.rb
@@ -2,7 +2,11 @@ require 'ralyxa'
 intent 'AddRelation' do
 	name = request.slot_value("Name").downcase
 	relatn = request.slot_value("Relation")
-	relation = Relation.new(:name => name, :relation_type => relatn)
-	relation.save
-	respond("#{name} is saved as your #{relatn}")
+
+	find_relation = Relation.where(:name => name)
+	if find_relation.empty?
+		relation = Relation.create(:name => name, :relation_type => relatn)
+		tell("#{name} is saved as your #{relatn}")
+	else
+		find_relation.update(:relation_type => relatn)
 end
--- intents/name_relation.rb
+++ intents/name_relation.rb
@@ -4,8 +4,8 @@ intent "NameRelation" do
 	relation = Relation.where(:name => name)
 
 	if relation.empty?
-		respond("Sorry you don't have that much relation in this dangerous world")
+		tell("Sorry you don't have that much relation in this dangerous world")
 	else
-		respond("#{relation[0].name} is your #{relation[0].relation_type}")
+		tell("#{relation[0].name} is your #{relation[0].relation_type}")
 	end
 end
