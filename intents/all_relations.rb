6bfe574d12d731e28c0ab1471d426ed3948601ba
Shubham Shiva Yadav <syadav.yadav32@gmail.com>
2018-02-19 22:40:39 +0530

    added new intent AllRelations

--- intents/all_relations.rb
+++ intents/all_relations.rb
@@ -3,6 +3,7 @@ intent "AllRelations" do
 	relations = Relation.all
 
 	relations.each do |relation|
+		relation_str = ""
 		relation_str = relation_str + "#{relations[0].name} is your #{relations[0].relation_type}. "
 	end
 
