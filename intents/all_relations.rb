4f2162e399c31a3596fdb28ebe96aa9899fd4cfe
Shubham Shiva Yadav <syadav.yadav32@gmail.com>
2018-02-19 22:59:38 +0530

    added new intent AllRelations

--- intents/all_relations.rb
+++ intents/all_relations.rb
@@ -2,10 +2,10 @@ require 'ralyxa'
 intent "AllRelations" do
 	relations = Relation.all
 
-	relations.each do |relation|
+	#relations.each do |relation|
 		relation_str = ""
-		relation_str = relation_str + "#{relation.name} is your #{relation.relation_type}. "
-	end
+		relation_str = relation_str + "#{relations[0].name} is your #{relations[0].relation_type}. "
+	#end
 
 	tell("#{relation_str}")
 
